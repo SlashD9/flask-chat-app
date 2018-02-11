b5c825ede8d219167c7af897eddff8d71931f4e9
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-11 15:05:59 +0000

    added the ability to add username as an argument and show that on the webpage

--- run.py
+++ run.py
@@ -3,9 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "<h1>Hello World</h1>"
+    """Main page for with instructions"""
+    return "To send a message use/USERNAME/MESSAGE"
     
+    
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
