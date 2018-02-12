1418db752997501dca40679648c86320e5fe0165
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-12 21:40:44 +0000

    Update chat functionality to read and write from txt files

--- run.py
+++ run.py
@@ -4,16 +4,20 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+def write_to_file(filename, data):
+    """Handle the processs of writing data to txt file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
 def add_messages(username, message):
-    """Add messages to the messages list"""
+    """Add messages to the messages text file"""
     now = datetime.now().strftime("%H:%M:%S")
-    message_dict = {"timestamp": now, "message": message, "from": username}
     #Write the chat message to the messages.txt file
-    with open("data/messages.txt", "a") as chat_list:
-        chat_list.writelines("({0}) {1} - {2}\n".format(
-            message_dict["timestamp"], 
-            message_dict["from"].title(), 
-            message_dict["message"]))
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"), 
+            username.title(), 
+            message))
     
     
 def get_all_messages():
@@ -27,9 +31,11 @@ def get_all_messages():
 @app.route("/", methods={"GET", "POST"})
 def index():
     """Main page for with instructions"""
+    
+    #Handle the POST request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.write(request.form["username"] + "\n")
+        
+        write_to_file("data/users.txt", request.form["username"] + "\n")
         return redirect(request.form["username"])
     return render_template("index.html")
     
